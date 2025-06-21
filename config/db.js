95f3dbb66e517221f3af87ff19e164e99b319322
Rohanisamazing <Rohanjambhulkar98@gmail.com>
2025-06-21 19:21:29 +0530

    db js changes

--- config/db.js
+++ config/db.js
@@ -4,6 +4,9 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       // No need for useUnifiedTopology or useNewUrlParser in modern drivers
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000,
     });
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
