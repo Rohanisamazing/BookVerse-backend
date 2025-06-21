012083ac69605ea9562b35384133cf472e6e9f1d
Rohanisamazing <Rohanjambhulkar98@gmail.com>
2025-06-21 20:46:47 +0530

    removed trailing slash from frontend url

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 
 
 const allowedOrigins = [
-  'https://book-verse-frontend-mauve.vercel.app/', // Replace with your actual frontend URL
+  'https://book-verse-frontend-mauve.vercel.app', // Replace with your actual frontend URL
   'http://localhost:3000' // For local development, optional
 ];
 
